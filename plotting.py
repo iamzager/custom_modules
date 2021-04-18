b87b175252afc043bfbf64ccaa58c7f1ca176fbb
iamzager <72935261+iamzager@users.noreply.github.com>
2021-04-18 18:49:10 +0300

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -7,8 +7,10 @@ import numpy as np
 import pandas as pd
 
 def annotate_bars(plot, ha='center', va='bottom', precision=None):
+    if type(plot) == tuple:
+        plot = plot[2]
     for p in plot.patches:
         x = p.get_bbox().get_points()[:,0]
         y = p.get_bbox().get_points()[1,1]
         caption = str(round(p.get_height(), precision))
-        plot.annotate(caption, (x.mean(), y), ha=ha, va=va)
+        plt.annotate(caption, (x.mean(), y), ha=ha, va=va)
